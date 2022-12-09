5e63bf558f6c0bd5f245fc913d874f77f2d969bf
Daniel Yakovlev <Daniel.yakovlev@gmail.com>
2022-12-09 13:39:17 -0800

    fixed pastry GetCost method

--- BakeryShop/Models/Pastry.cs
+++ BakeryShop/Models/Pastry.cs
@@ -1,9 +1,11 @@
+using System;
+
 namespace BakeryShop.Models
 {
   public class Pastry
   {
     // properties, constructors, methods, etc. go here
-    public int Count {get; set;}
+    public int Count { get; set; }
 
     public Pastry(int Count)
     {
@@ -12,7 +14,41 @@ namespace BakeryShop.Models
 
     public int GetCost()
     {
-      return this.Count * 2;
+
+
+      int cost = 0;
+      int tempCount = Count;
+      while (tempCount > 0)
+      {
+        for (int i = 6; i > 0; i--)
+        {
+          if (tempCount - i >= 0)
+          {
+            switch (i)
+            {
+              case 6:
+                cost += 10;
+                break;
+              case 5:
+                cost += 9;
+                break;
+              case 4:
+                cost += 7;
+                break;
+              case 3:
+                cost += 5;
+                break;
+              default:
+                cost += (i * 2);
+                break;
+            }
+            tempCount -= i;
+
+            break;
+          }
+        }
+      }
+      return cost;
     }
   }
 }
