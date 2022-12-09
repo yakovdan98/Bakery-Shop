0abfea59243f68704567563eb7a40ed4682fc5b7
Daniel Yakovlev <Daniel.yakovlev@gmail.com>
2022-12-09 15:31:07 -0800

    changed console output

--- BakeryShop/BakeryShop.cs
+++ BakeryShop/BakeryShop.cs
@@ -6,7 +6,8 @@ namespace BakeryShop
   class Program
   {
     public static void Main(string[] args)
-    {
+    { 
+      Console.WriteLine("Welcome to Pierre's Bakery!\nBread is $5 and is running a buy 1 get one free special\nThe price for Pastries is Buy 1 for $2 or 3 for $5. Four pastries costs $7, five pastries costs $9, and six pastries costs $10.");
       Console.WriteLine("How much bread do you want?");
       int breadCount = int.Parse(Console.ReadLine());
       Bread bread = new Bread(breadCount);
