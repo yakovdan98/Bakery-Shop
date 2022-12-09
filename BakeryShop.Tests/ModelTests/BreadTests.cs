eed8e3c4f18e068cff7808b03aab691e9c102fea
Daniel Yakovlev <Daniel.yakovlev@gmail.com>
2022-12-09 10:53:20 -0800

    refactored pastry tests

--- BakeryShop.Tests/ModelTests/BreadTests.cs
+++ BakeryShop.Tests/ModelTests/BreadTests.cs
@@ -23,35 +23,33 @@ namespace BakeryShop.Tests
       Assert.AreEqual(10, bread.Cart());
     }
     [TestMethod]
-    public void GetCost_ReturnsCost_Cost()
+    public void BreadGetCost_ReturnsCost_Cost()
     {
       // any necessary logic to prep for test; instantiating new classes, etc.
       // Template: ``` Assert.AreEqual(EXPECTED_RESULT, CODE_TO_TEST); ```
       Pastry bread = new Bread(0);
       Assert.AreEqual(0, bread.GetCost());
-
-
     }
     [TestMethod]
-    public void GetCostOne_ReturnsCost_Cost()
+    public void BreadGetCostOne_ReturnsCost_Cost()
     {
       Pastry bread1 = new Bread(1);
       Assert.AreEqual(5, bread1.GetCost());
     }
     [TestMethod]
-    public void GetCostTwo_ReturnsCost_Cost()
+    public void BreadGetCostTwo_ReturnsCost_Cost()
     {
       Pastry bread2 = new Bread(2);
       Assert.AreEqual(10, bread2.GetCost());
     }
     [TestMethod]
-    public void GetCostDiscount_ReturnsCost_Cost()
+    public void BreadGetCostDiscount_ReturnsCost_Cost()
     {
       Pastry bread3 = new Bread(3);
       Assert.AreEqual(10, bread3.GetCost());
     }
     [TestMethod]
-    public void GetCostMore_ReturnsCost_Cost()
+    public void BreadGetCostMore_ReturnsCost_Cost()
     {
       Pastry bread4 = new Bread(4);
       Assert.AreEqual(15, bread4.GetCost());
@@ -79,27 +77,55 @@ namespace BakeryShop.Tests
       Assert.AreEqual(10, pastry.Cart());
     }
     [TestMethod]
-    public void GetCost_ReturnsCost_Cost()
+    public void PastryGetCost_ReturnsCost_Cost()
     {
       // any necessary logic to prep for test; instantiating new classes, etc.
       // Template: ``` Assert.AreEqual(EXPECTED_RESULT, CODE_TO_TEST); ```
       Pastry pastry = new Pastry(0);
-      Pastry pastry1 = new Pastry(1);
-      Pastry pastry2 = new Pastry(2);
-      Pastry pastry3 = new Pastry(3);
-      Pastry pastry4 = new Pastry(4);
-      Pastry pastry5 = new Pastry(5);
-      Pastry pastry6 = new Pastry(6);
-      Pastry pastry7 = new Pastry(7);
       Assert.AreEqual(0, pastry.GetCost());
-      Assert.AreEqual(2, pastry1.GetCost());
-      Assert.AreEqual(4, pastry2.GetCost());
-      Assert.AreEqual(5, pastry3.GetCost());
-      Assert.AreEqual(7, pastry4.GetCost());
-      Assert.AreEqual(9, pastry5.GetCost());
-      Assert.AreEqual(10, pastry6.GetCost());
-      Assert.AreEqual(12, pastry7.GetCost());
 
+    }
+    [TestMethod]
+    public void PastryGetCostOne_ReturnsCost_Cost()
+    {
+      Pastry pastry1 = new Pastry(1);
+      Assert.AreEqual(2, pastry1.GetCost());
+    }
+    [TestMethod]
+    public void PastryGetCostTwo_ReturnsCost_Cost()
+    {
+      Pastry pastry2 = new Pastry(2);
+      Assert.AreEqual(4, pastry2.GetCost());
+    }
+    [TestMethod]
+    public void PastryGetCostThree_ReturnsCost_Cost()
+    {
+      Pastry pastry3 = new Pastry(3);
+      Assert.AreEqual(5, pastry3.GetCost());
+    }
+    [TestMethod]
+    public void PastryGetCostFour_ReturnsCost_Cost()
+    {
+      Pastry pastry4 = new Pastry(4);
+      Assert.AreEqual(7, pastry4.GetCost());
+    }
+    [TestMethod]
+    public void PastryGetCostFive_ReturnsCost_Cost()
+    {
+      Pastry pastry5 = new Pastry(5);
+      Assert.AreEqual(9, pastry5.GetCost());
+    }
+    [TestMethod]
+    public void PastryGetCostSix_ReturnsCost_Cost()
+    {
+      Pastry pastry6 = new Pastry(6);
+      Assert.AreEqual(10, pastry6.GetCost());
+    }
+    [TestMethod]
+    public void PastryGetCostSeven_ReturnsCost_Cost()
+    {
+      Pastry pastry7 = new Pastry(7);
+      Assert.AreEqual(12, pastry7.GetCost());
     }
   }
 }
